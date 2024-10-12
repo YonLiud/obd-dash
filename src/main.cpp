04b6c679afe6ec6a925be22da543f5ae96703576
Yon <ejliud@gmail.com>
2024-10-12 21:15:21 +0300

    feat: Add ELFduino library dependency
    - Add dependency
    - Add basic connection code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,52 @@
 #include <Arduino.h>
+#include <ELMduino.h>
+#include <BluetoothSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothSerial SerialBT;
+
+#define SERIAL_BAUD 115200
+#define ELM_PORT    SerialBT
+#define DEBUG_PORT  Serial
+#define LED_PIN     2
+
+const bool DEBUG_MODE     = true;
+const int  ELM_TIMEOUT_MS = 1000;
+const bool HALT_ON_ERROR  = false;
+
+ELM327 elm;
+
+uint32_t rpm;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  DEBUG_PORT.begin(SERIAL_BAUD);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  ELM_PORT.begin("obdScanner", true);
+  DEBUG_PORT.println("Bluetooth device started: obdScanner");
+
+  if(!ELM_PORT.connect("OBDII"))
+  {
+    DEBUG_PORT.println("Failed to connect to OBDII device - Phase 1");
+    return;
+  }
+
+  if(!elm.begin(ELM_PORT, DEBUG_MODE, ELM_TIMEOUT_MS))
+  {
+    DEBUG_PORT.println("Failed to connect to OBDII device - Phase 2");
+    return;
+  }
+
+  DEBUG_PORT.println("Connected to OBDII device");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float tempRPM = elm.rpm();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (elm.nb_rx_state == ELM_SUCCESS)
+  {
+    rpm = (uint32_t)tempRPM;
+    Serial.print("RPM: "); Serial.println(rpm);
+  }
+  else if (elm.nb_rx_state != ELM_GETTING_MSG)
+    elm.printError();
 }
